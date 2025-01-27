7e0eae7aff1ee19c108a1fcba8a6065408d64aa8
Arsalan831 <arsalanbashir831@gmail.com>
2025-01-27 21:28:36 +0500

    layout change

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,17 +8,17 @@ export default function AuthLayout({
 }>) {
   return (
     <>
-    <div className="flex min-h-screen bg-[#F8F8FF]">
-      <div className="flex-1 md:basis-1/2 px-8 py-12">
-        <div className="mx-auto flex h-full max-w-md flex-col items-center justify-center space-y-8">
-          <Logo />
-          {children}
+      <div className="flex min-h-screen bg-[#F8F8FF]">
+        <div className="flex-1 md:basis-1/2 px-8 py-12">
+          <div className="mx-auto flex h-full max-w-md flex-col items-center justify-center space-y-8">
+            <Logo />
+            {children}
+          </div>
+        </div>
+        <div className="md:basis-1/2 p-8 hidden lg:flex items-center justify-center">
+          <AuthSlider />
         </div>
       </div>
-      <div className="md:basis-1/2 p-8 hidden lg:flex items-center justify-center">
-        <AuthSlider />
-      </div>
-    </div>
     </>
   );
 }
