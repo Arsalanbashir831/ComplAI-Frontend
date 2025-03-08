48ddc53def5eb3cc3b5104171f8148b6bba8e410
Arsalan831 <arsalanbashir831@gmail.com>
2025-03-08 09:16:31 +0500

    update subscription caching issue

--- src/app/(dashboard)/subscription/page.tsx
+++ src/app/(dashboard)/subscription/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { API_ROUTES } from '@/constants/apiRoutes';
 import { useUserContext } from '@/contexts/user-context';
 import { Elements } from '@stripe/react-stripe-js';
@@ -10,17 +11,16 @@ import {
   useQuery,
   useQueryClient,
 } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
+import type { PaymentCard, Plan, Subscription } from '@/types/subscription';
+import apiCaller from '@/config/apiCaller';
+import { formatDateLocal } from '@/lib/utils';
 import LoadingSpinner from '@/components/common/loading-spinner';
 import DashboardHeader from '@/components/dashboard/dashboard-header';
 import { PaymentMethod } from '@/components/dashboard/subscription/payment-method';
 import { PricingCard } from '@/components/dashboard/subscription/pricing-card';
 import { SubscriptionInfo } from '@/components/dashboard/subscription/subscription-info';
-import apiCaller from '@/config/apiCaller';
-import { formatDateLocal } from '@/lib/utils';
-import type { PaymentCard, Plan, Subscription } from '@/types/subscription';
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
@@ -169,21 +169,23 @@ export default function SubscriptionPage() {
   });
 
   // New query to fetch user subscriptions data
-  const { data: userSubscriptions, isLoading: subscriptionsLoading , refetch } = useQuery(
-    {
-      queryKey: ['userSubscriptions'],
-      queryFn: fetchUserSubscriptions,
-      staleTime: 1000 * 60 * 5,
-    }
-  );
+  const {
+    data: userSubscriptions,
+    isLoading: subscriptionsLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ['userSubscriptions'],
+    queryFn: fetchUserSubscriptions,
+    staleTime: 1000 * 60 * 5,
+  });
 
   // Log the subscriptions data when it becomes available
   useEffect(() => {
     if (userSubscriptions) {
       console.log('User Subscriptions:', userSubscriptions);
-      refetch()
+      refetch();
     }
-  }, [userSubscriptions,refetch]);
+  }, [userSubscriptions, refetch]);
 
   const purchaseTokensMutation = useMutation({
     mutationFn: async (productId: number) => {
