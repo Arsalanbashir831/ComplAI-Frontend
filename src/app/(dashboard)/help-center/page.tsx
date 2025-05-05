df93907cddc0c611f9e73ec91d5a8f2a4a1fc2c6
Arsalan831 <arsalanbashir831@gmail.com>
2025-05-05 13:17:40 +0500

    help and support UI

---
+++ src/app/(dashboard)/help-center/page.tsx
@@ -0,0 +1,25 @@
+// app/dashboard/help-support/page.tsx
+'use client';
+
+import DashboardHeader from '@/components/dashboard/dashboard-header';
+import SupportForm from '@/components/dashboard/help-support/help-form-support';
+
+export default function Page() {
+  return (
+    <>
+    <div className='p-5 flex flex-col gap-y-10'>
+    <DashboardHeader
+                title="Help & Support"
+                subtitle="Please Menion any issues you are facing"
+              />
+
+<div >
+       
+       <SupportForm />
+     </div>
+    </div>
+     
+    </>
+   
+  );
+}
---
+++ src/components/dashboard/help-support/help-form-support.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
+
+
+
+export default function SupportForm() {
+  const [selectedVersion, setSelectedVersion] = useState('');
+
+  return (
+    <div className="gap-8 px-4 md:px-0">
+      {/* Form: Displayed first on mobile, second on desktop */}
+      <div className="space-y-4 flex-1 order-1 md:order-2">
+        <Input placeholder="Your full name" className="py-6" />
+        <Input type="email" placeholder="Your email" className="py-6" />
+
+        {/* Version Dropdown */}
+        <Select onValueChange={setSelectedVersion}>
+          <SelectTrigger
+            className={cn(!selectedVersion && 'text-muted-foreground')}
+          >
+            <SelectValue placeholder="Version" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="version1">Compl-AI-v1</SelectItem>
+            {/* <SelectItem value="version2">Version 2</SelectItem> */}
+          </SelectContent>
+        </Select>
+
+        <Textarea placeholder="Description" className="h-[200px] py-6" />
+        <Button className="w-full transition-all duration-300 ease-in-out ">
+          Submit Support Request
+        </Button>
+      </div>
+
+    
+    </div>
+  );
+}
