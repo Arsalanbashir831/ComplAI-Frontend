e4bf4a7571529724db670415411710e068cc2e1b
Saadaan-Hassan <webmaker9d@gmail.com>
2025-12-11 17:13:10 +0500

    Remove the unused packages and files

--- src/components/common/DocxViewer.tsx
+++ src/components/common/DocxViewer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import mammoth from 'mammoth';
+import { useEffect, useState } from 'react';
 
 export default function DocxViewer({
   filePath,
--- src/types/doc-compliance.ts
+++
@@ -1,7 +0,0 @@
-export interface ComplianceResult {
-  original: string | null | undefined;
-  compliant: boolean;
-  suggestion: string;
-  reason: string;
-  citations: unknown[];
-}
